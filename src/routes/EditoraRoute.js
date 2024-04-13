129d84b29be4b6c725d125009c3d7184a6961f70
thyagoviana <contadothyago@gmail.com>
2024-04-12 22:02:09 -0300

    fix

--- src/routes/EditoraRoute.js
+++ src/routes/EditoraRoute.js
@@ -6,7 +6,7 @@ const editoraController = new EditoraController();
 const routes = Router();
 
 routes.get('/editoras',(req,res) => editoraController.getAll(req,res)),
-routes.get('/editoras/:id',(req,res) => editoraController.getPorID(req,res));
+routes.get('/editoras/:id',(req,res) => editoraController.getPorId(req,res));
 routes.post('/editoras',(req,res) => editoraController.criaNovo(req,res));
 routes.put('/editoras/:id',(req,res) => editoraController.atualiza(req,res));
 routes.delete('/editoras/:id',(req,res) => editoraController.exclui(req,res));
